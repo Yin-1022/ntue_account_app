5306458aa3226dc4cbeea0f2dcca245768b13994
Yin-1022 <kv.huang931022@gmail.com>
2025-06-09 22:38:25 +0800

    calPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'view/cal_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +10,8 @@ class MyApp extends StatelessWidget
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(home: CalPage());
   }
 }
