0a78b245fc0ee75da1058c267d6ff413eb535743
ben <ouo.joy1028@gmail.com>
2025-06-09 21:33:51 +0800

    home UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,50 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget
-{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Center(child: const Text('NUTE Account App'))),
+
+        body: Column(
+          children: [
+            Container(height: 30),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+                Column(
+                  children: [
+                    Text('monthly', style: TextStyle(fontSize: 25)),
+                    Text('expenses:', style: TextStyle(fontSize: 25)),
+                  ],
+                ),
+
+                SizedBox(width: 150),
+
+                Column(
+                  children: [
+                    Text('monthly', style: TextStyle(fontSize: 25)),
+                    Text('income:', style: TextStyle(fontSize: 25)),
+                  ],
+                ),
+              ],
+            ),
+
+            Container(height: 100),
+            Container(width: 400, height: 2, color: Colors.grey,),
+            Container(height: 500),
+            Container(width: 400, height: 2, color: Colors.grey,),
+            Container(height: 100),
+            Icon(Icons.home, size: 50,)
+          ],
+        ),
+      ),
+    );
   }
 }
