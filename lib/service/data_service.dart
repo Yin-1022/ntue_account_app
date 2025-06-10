3b91a5912929e8497002b09922637b23c7660474
Hung <a921311973@gmail.com>
2025-06-10 19:19:28 +0800

    service

--- lib/service/data_service.dart
+++ lib/service/data_service.dart
@@ -1,59 +1,28 @@
-import "../model/data.dart";
-class DataService{
-  final List <Data> _data = [
+import '../model/data.dart';
+
+class DataService {
+  final List<Data> _datas = [
     Data(
         dateTime: DateTime(
             2025,
             01,
-            03,
-            0,
-            0,
-            0,
-            0,
-            0),
+            03
+        ),
         title: '牛肉麵',
         type: 'food',
         dollar: 200
     ),
-    Data(dateTime: DateTime(
+    Data(
+        dateTime: DateTime(
         2025,
         01,
-        04,
-        0,
-        0,
-        0,
-        0,
-        0),
-        title: '',
-        type: '',
-        dollar: 200
+        04
+    ),
+        title: '紅茶',
+        type: 'drink',
+        dollar: 100
     )
   ];
 
-  List <Data> get data => _data;
-  int updateMonthlyExpenses({required String year, required String month}){
-    int dollar=0;
-    for (int i = 0; i < _data.length; i++) {
-      String entryYear = _data[i].dateTime.year.toString();
-      String entryMonth = _data[i].dateTime.month.toString();
-      int entryDollar = _data[i].dollar;
-      if(entryYear==year && entryMonth==month && entryDollar<0){
-        dollar=dollar+entryDollar;
-      }
-    }
-    return dollar;
-  }
-  int updateMonthlyIncome({required String year, required String month}){
-    int dollar=0;
-    for (int i = 0; i < _data.length; i++) {
-      String entryYear = _data[i].dateTime.year.toString();
-      String entryMonth = _data[i].dateTime.month.toString();
-      int entryDollar = _data[i].dollar;
-      if(entryYear==year && entryMonth==month && entryDollar>0){
-        dollar=dollar+entryDollar;
-      }
-    }
-    return dollar;
-  }
+  List<Data> get datas => _datas;
 }
-
